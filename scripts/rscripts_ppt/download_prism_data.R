f57ae728670ee7c69d66b768bc904edb0ba55619
David Leifer <davidleifer@Davids-MacBook.local>
2021-05-16 01:58:14 -0500

    commit 76

--- scripts/rscripts_ppt/download_prism_data.R
+++ scripts/rscripts_ppt/download_prism_data.R
@@ -2,7 +2,6 @@ library(prism)
 
 cwd <- getwd()
 end_string <- '/data/ppt'
-path_concat <- paste(cwd, end_string, sep='')
+path_concat <- paste(cwd, end_string, sep = "")
 prism_set_dl_dir(path_concat)
-
 get_prism_monthlys(type = "ppt", year = 1981:2014, mon = 1, keepZip = FALSE)
--- scripts/rscripts_tmean/download_prism_data.R
+++ scripts/rscripts_tmean/download_prism_data.R
@@ -2,7 +2,6 @@ library(prism)
 
 cwd <- getwd()
 end_string <- '/data/tmean'
-path_concat <- paste(cwd, end_string, sep='')
-
+path_concat <- paste(cwd, end_string, sep = "")
 prism_set_dl_dir(path_concat)
 get_prism_monthlys(type = "tmean", year = 1981:2014, mon = 1, keepZip = FALSE)
