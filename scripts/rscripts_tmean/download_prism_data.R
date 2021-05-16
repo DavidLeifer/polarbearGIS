378356bb41b2f4774afae907cf4481e46e02bff9
David Leifer <davidleifer@Davids-MacBook.local>
2021-05-16 01:53:10 -0500

    commit 74

--- scripts/rscripts_tmean/download_prism_data.R
+++ scripts/rscripts_tmean/download_prism_data.R
@@ -2,7 +2,7 @@ library(prism)
 
 cwd <- getwd()
 end_string <- '/data/tmean/'
-path_concat <- paste(cwd, end_string)
+path_concat <- paste(cwd, end_string, sep='')
 
 prism_set_dl_dir(path_concat)
 get_prism_monthlys(type = "tmean", year = 1981:2014, mon = 1, keepZip = FALSE)
