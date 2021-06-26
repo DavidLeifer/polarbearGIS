eccb06a7b2abdd7a17ec7d46f8964fc32b9c3611
David Leifer <davleifer@gmail.com>
2021-06-26 13:50:53 -0500

    commit 106

--- final_project/scripts/save_csv.py
+++
@@ -1,9 +0,0 @@
-#save as csv
-
-import json, os
-import pandas as pd
-
-fname = "/Users/davidleifer/Documents/20170101-20190604/Geog531/final_project/data_collection/energy20190206T153016.txt_geo_named.json_geo_sent.json"
-
-df = pd.read_json(fname, lines=False)
-df.to_csv('/Users/davidleifer/Documents/20170101-20190604/Geog531/final_project/data_collection/energy20190206T153016.txt_geo_named.json_geo_sent.csv')
--- scripts/python_weather/geosent_2rounded.py
+++ scripts/python_weather/geosent_2rounded.py
@@ -8,14 +8,11 @@ import fiona
 import matplotlib.pyplot as plt
 import datetime as dt
 
-#df = pd.read_json('/Users/davidleifer/Documents/20170101-20190604/Geog531/GeoStreamable/data/energy20190204T160315.txt_geo_named.json_geo_sent.json', lines=False)
-#for i, row in df.iterrows():
-#    print(row)
-# Read vector data in as GeoDataFrame AKA poly, use this to select points in polygon in the for loop
-poly1 = gpd.read_file("/Users/davidleifer/Documents/20170101-20190604/Geog531/GeoStreamable/data/EastCoast.shp")
+cwd = os.getcwd()
+poly1 = gpd.read_file(cwd + "/data/polar_radar_unsplit/EastCoast.shp")
 poly = poly1.loc[poly1['NAME'] == "Kentucky"]
 #https://stackoverflow.com/questions/20906474/import-multiple-csv-files-into-pandas-and-concatenate-into-one-dataframe
-path = '/Users/davidleifer/Documents/20170101-20190604/Geog531/GeoStreamable/data'
+path = cwd + '/data/polar_radar_unsplit'
 all_files = glob(os.path.join(path, "*geo_sent.json"))
 #loop over glob list of files
 for i in all_files:
@@ -45,13 +42,4 @@ for i in all_files:
                            'lang', 'filter_level', 'entities', 'quoted_status_permalink', 'quoted_status',
                            'place', 'coordinates', 'geo', 'user', 'in_reply_to_screen_name'], axis=1)
         group.to_file(str(index) + ".geojson", driver="GeoJSON")
-
-#save as CSV file
-#filename = '/Users/davidleifer/Documents/20170101-20190604/Geog531/final_project/data/polarVortex_intersect_rounded.csv'
-#intersect.to_csv(filename, index=False, encoding='utf-8')
-
-
-
-
-
-
+print("geosent_2rounded.py finished")
